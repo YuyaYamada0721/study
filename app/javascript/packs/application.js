0786c03f439cad357bc95845e7ca9b6e0324ac76
yuya yamada <yuya19920721@gmail.com>
2021-09-12 18:11:59 +0900

    Bootstrap5設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,12 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// 作成したファイルをwebpackerに読み込ませます。
+import "../stylesheets/application"
+import "../js/hoge.js"
+
+// - 検証用 -
+// app/javascript/images/フォルダー内の001.jpeg画像を
+// webpackerに読み込ませます。
+const images = require.context('../images', true)
